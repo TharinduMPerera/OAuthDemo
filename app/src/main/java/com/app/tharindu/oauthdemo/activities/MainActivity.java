83306d7d42212db6710e2aac18c26ec7286dd388
Tharindu Perera <tharindu.madushanka@my.sliit.lk>
2018-10-26 05:10:46 +0530

    - handled authorization callback

--- app/src/main/java/com/app/tharindu/oauthdemo/activities/MainActivity.java
+++ app/src/main/java/com/app/tharindu/oauthdemo/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.app.tharindu.oauthdemo.R;
 import com.app.tharindu.oauthdemo.helper.Consts;
@@ -25,5 +26,21 @@ public class MainActivity extends AppCompatActivity {
         Intent githubLogin = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/login/oauth/authorize" +
                 "?client_id=" + Consts.clientID + "&scope=repo&redirect_uri=" + Consts.redirectUri));
         startActivity(githubLogin);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Uri uri = getIntent().getData();
+        if (uri != null && uri.toString().startsWith(Consts.redirectUri)) {
+            // use the parameter your API exposes for the code ("code")
+            String code = uri.getQueryParameter("code");
+            String error;
+            if (code != null) {
+
+            } else if ((error = uri.getQueryParameter("error")) != null) {
+                Toast.makeText(this, "Something went wrong! Error: " + error, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
