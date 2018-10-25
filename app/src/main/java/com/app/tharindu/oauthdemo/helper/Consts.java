4a415cd508688a732cdbe0582600e323699b8430
Tharindu Perera <tharindu.madushanka@my.sliit.lk>
2018-10-25 23:55:18 +0530

    - added app const file

---
+++ app/src/main/java/com/app/tharindu/oauthdemo/helper/Consts.java
@@ -0,0 +1,9 @@
+package com.app.tharindu.oauthdemo.helper;
+
+public class Consts {
+
+    final public static String redirectUri = "tharinduapps://callback";
+    final public static String clientID = "fe93357e7ceec8654e69";
+    final public static String clientSecret = "cf48ad2162721c85b16bdba70823750350e20639";
+
+}
