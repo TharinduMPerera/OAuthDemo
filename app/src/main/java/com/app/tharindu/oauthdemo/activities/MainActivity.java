83fb2b92cf214e6e02ecf2a070ba659fd4aae5a0
Tharindu Perera <tharindu.madushanka@my.sliit.lk>
2018-10-25 23:55:56 +0530

    - added login button and onclick listener

--- app/src/main/java/com/app/tharindu/oauthdemo/activities/MainActivity.java
+++ app/src/main/java/com/app/tharindu/oauthdemo/activities/MainActivity.java
@@ -1,9 +1,15 @@
 package com.app.tharindu.oauthdemo.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.app.tharindu.oauthdemo.R;
+import com.app.tharindu.oauthdemo.helper.Consts;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.githubLoginBtn)
+    public void showGithubLogin() {
+        Intent githubLogin = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/login/oauth/authorize" +
+                "?client_id=" + Consts.clientID + "&scope=repo&redirect_uri=" + Consts.redirectUri));
+        startActivity(githubLogin);
     }
 }
