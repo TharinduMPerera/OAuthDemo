7cd875865ba1f5bc81150b6fc9a1d74dafc86402
Tharindu Perera <tharindu.madushanka@my.sliit.lk>
2018-10-26 05:31:38 +0530

    - changed base url

--- app/src/main/java/com/app/tharindu/oauthdemo/services/api/ApiUtils.java
+++ app/src/main/java/com/app/tharindu/oauthdemo/services/api/ApiUtils.java
@@ -5,7 +5,7 @@ import com.app.tharindu.oauthdemo.helper.Consts;
 public class ApiUtils {
     private ApiUtils(){}
 
-    public static final String BASE_URL = Consts.domainURL+"api/v1/";
+    private static final String BASE_URL = Consts.domainURL;
 
     public static APIService getAPIService(){
         return RetrofitClient.getClient(BASE_URL).create(APIService.class);
