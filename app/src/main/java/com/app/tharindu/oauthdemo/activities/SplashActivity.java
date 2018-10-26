a62df345eceefdc2b034332b95d816375fd60187
Tharindu Perera <tharindu.madushanka@my.sliit.lk>
2018-10-26 05:50:38 +0530

    - code enhanced

--- app/src/main/java/com/app/tharindu/oauthdemo/activities/SplashActivity.java
+++ app/src/main/java/com/app/tharindu/oauthdemo/activities/SplashActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.app.tharindu.oauthdemo.R;
-
 public class SplashActivity extends AppCompatActivity {
 
     @Override
